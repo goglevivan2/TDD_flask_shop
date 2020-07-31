054746d78a1059c0750a325038b45f1aa17a6047
ivan <ivangoglev@protonmail.com>
2020-07-31 17:40:09 +0300

    'FT002_about_shop_page'

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Home Page'
-
+    return render_template('index.html')
+@app.route('/about')
+def adout():
+    return render_template('about.html')
 if __name__ == "__main__":
     app.run(debug=True)
